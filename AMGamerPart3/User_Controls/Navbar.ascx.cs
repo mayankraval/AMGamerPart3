3a369cc2e6bf6358c8146730bb7aa59bfd7198f9
mayankraval <mayank.raval8293@gmail.com>
2016-06-17 00:07:42 -0500

    Add project files.

---
+++ AMGamerPart3/User_Controls/Navbar.ascx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AMGamerPart3.User_Controls
+{
+    public partial class Navbar : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SetActivePage();
+        }
+
+        /**
+         * This method below will make the pages/link 'active' 
+         * with the css class
+         * @version: 0.0.1 - added SetActivePage method
+         * @private method SetActivePage
+         * @return {void}
+         */
+        private void SetActivePage()
+        {
+            switch (Page.Title)
+            {
+                case "Home Page":
+                    home.Attributes.Add("class", "active");
+                    break;
+                case "Login":
+                    login.Attributes.Add("class", "active");
+                    break;
+                case "Register":
+                    register.Attributes.Add("class", "active");
+                    break;
+                case "Game":
+                    game.Attributes.Add("class", "active");
+                    break;
+                case "Game Details":
+                    editgame.Attributes.Add("class", "active");
+                    break;
+                case "Contact":
+                    contact.Attributes.Add("class", "active");
+                    break;
+            }
+        }
+    }
+}
